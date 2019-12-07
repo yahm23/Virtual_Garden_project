c9ffc2161dbde96a085860237f813a3bc3f6daad
Aresky Berkane <areskyberkane@gmail.com>
2019-12-07 17:23:50 +0000

    create MongoClient

--- Server/server.js
+++ Server/server.js
@@ -6,3 +6,14 @@ app.use(cors());
 
 const parser = require('body-parser');
 app.use(parser.json());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+.then(client => {
+  const db = client.db('virtualGarden');
+  const userCollection = users.collection('users');
+  const userRouter = createRouter(userCollection);
+  app.use('/home', userRouter); 
+})
