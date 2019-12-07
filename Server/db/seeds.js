25a19f49c895134b0376d434731dcefd60954654
Aresky Berkane <areskyberkane@gmail.com>
2019-12-07 17:23:25 +0000

    add scketch of the db structure

---
+++ Server/db/seeds.js
@@ -0,0 +1,28 @@
+use virtualGarden;
+db.dropDatabase();
+
+// db.users.instertMany([
+//   {
+//     id: 'provided by mongodb',
+//     name:"",
+//     email:"",
+//     gardens:[
+//       {
+//         id: 'provided by mongodb',
+//         location_id:"",
+//         woeid:'number'
+//         ,
+//         plants:[
+//           {
+//             apiData: plant_id:'number',
+//             simulationData:{
+//               birthDate: "",  
+//               waterLevel:""  
+//             }
+//           }
+//         ]
+//       }
+//     ]
+//   }
+// ]);
+
