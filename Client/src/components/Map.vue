23582ac1f5c8f5ca00fec3cf7c93a859f7e21272
Yousef <yahmed1996a@gmail.com>
2019-12-07 16:51:59 +0000

    virtual garden linked from map

--- Client/src/components/Map.vue
+++ Client/src/components/Map.vue
@@ -1,13 +1,18 @@
 <template >
   <div class="">
     <p>Map connected</p>
+    <virtual-garden> </virtual-garden>
   </div>
 </template>
 
 <script>
+import VirtualGarden from '@/components/VirtualGarden.vue'
 
 export default {
-  nmae:'map-environment'
+  nmae:'map-environment',
+  components:{
+    'virtual-garden':VirtualGarden
+  }
 }
 </script>
 
---
+++ Client/src/components/VirtualGarden.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="">
+    <p>Virtual Garden connected</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'virtual-garden'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
