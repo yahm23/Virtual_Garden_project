35991722a326518d233ab6dc36e1aa0f9145aa1e
Aresky Berkane <areskyberkane@gmail.com>
2019-12-07 19:20:10 +0000

    add get by id request

--- Server/helpers/create_router.js
+++ Server/helpers/create_router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ObjectId = require('mongodb').ObjectId;
 
 const createRouter = function(collection){
-  
+
   const router = express.router();
   router.get('/', (req,res) => {
     collection
@@ -15,7 +15,19 @@ const createRouter = function(collection){
       res.json({status: 500, error: err});
     });
   });
-  
+
+  router.get('/:id', (req, res) => {
+    id = req.params.id;
+    collection
+    .findOne({_id:ObjectId(id) })
+    .then(doc => res.json(doc))
+    .catch( (err) => {
+      console.error(err);
+      res.status(500);
+      res.json({status: 500, error: err});
+    });
+  });
+
   router.delete('/:id', (req,res) => {
     const id = req.params.id;
     collection
@@ -26,9 +38,11 @@ const createRouter = function(collection){
       res.status(500);
       res.json({status: 500, error: err});
     });
-  });  
-  
-  
+  });
+
+
+
+
   return router
 };
 
