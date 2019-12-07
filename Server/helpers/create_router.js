6ce1d02c96425d4f4621596f3616084d652e1f47
Aresky Berkane <areskyberkane@gmail.com>
2019-12-07 18:45:04 +0000

    add a get request

---
+++ Server/helpers/create_router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
+
+const createRouter = function(collection){
+  
+  const router = express.router();
+  
+  router.get('/', (req,res) => {
+    collection
+    .find()
+    .toArray()
+    .then((docs) => res.json(docs))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({status: 500, error: err});
+    });
+  });
+  
+  
+}
+
+module.exports = createRouter;
