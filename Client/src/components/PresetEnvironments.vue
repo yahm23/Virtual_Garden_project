f7a70ed815f03002b532b3b3268ce7ec122146c4
Yousef <yahmed1996a@gmail.com>
2019-12-07 16:54:24 +0000

    Virtual garden linked to preset environment

--- Client/src/components/PresetEnvironments.vue
+++ Client/src/components/PresetEnvironments.vue
@@ -1,12 +1,18 @@
 <template >
   <div >
-    Preset-environment file linked
+    <p>Preset-environment file linked</p>
+    <virtual-garden> </virtual-garden>
   </div>
 </template>
 
 <script>
+import VirtualGarden from '@/components/VirtualGarden.vue'
+
 export default {
   name:'preset-environment',
+  components:{
+    'virtual-garden':VirtualGarden
+  }
 
 }
 </script>
