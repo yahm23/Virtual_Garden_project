cbdfd80123abb5caf9803ea363a16bcb7f2c03ae
Aresky Berkane <areskyberkane@gmail.com>
2019-12-08 14:52:37 +0000

    structure problem solved

--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const app = express();
+const parser = require('body-parser');
 
 const cors = require('cors');
+
 app.use(cors());
 
-const parser = require('body-parser');
 app.use(parser.json());
-
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
 
 MongoClient.connect('mongodb://localhost:27017')
 .then(client => {
   const db = client.db('virtualGarden');
-  const userCollection = users.collection('users');
-  const userRouter = createRouter(userCollection);
-  app.use('/home', userRouter); 
+  const userCollection = db.collection('users');  
+  app.use('/api/users', createRouter(userCollection)); 
 })
 .catch(console.error);
 
