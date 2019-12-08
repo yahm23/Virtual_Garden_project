b75ecbbbcc22db9d14277c23fa0a79492882023c
Eduardo Gizmo <eduardogimenezmoreno@gmail.com>
2019-12-08 11:27:42 +0000

    Map component working

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-
+    <map-fullscreen/>
   </div>
 </template>
 
 <script>
+import MapFullScreen from './components/Map.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    "map-fullscreen": MapFullScreen
   }
 }
 </script>
 
 <style>
-
+body {
+    margin: 35px;
+    border: 6px solid #222224;
+    box-sizing: border-box;
+    height: calc(100vh - 70px);
+}
 </style>
---
+++ Client/src/components/Map.vue
@@ -0,0 +1,62 @@
+<template lang="html">
+  <div class="full-map">
+
+    <l-map :zoom="zoom"
+           :min-zoom="minZoom"
+           :max-zoom="maxZoom"
+           :position="zoomPosition">
+        <l-tile-layer :url="url"></l-tile-layer>
+        <l-marker :lat-lng="marker"
+                  :draggable="draggable"
+                  :icon="icon"
+                  ></l-marker>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { icon, latLngBounds } from "leaflet";
+import {LMap, LTileLayer, LMarker ,
+
+    LControlZoom,
+
+} from 'vue2-leaflet';
+
+export default {
+    name: 'map-fullscreen',
+    components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LControlZoom,
+},
+    data: function() {
+      return {
+        maxZoom: 10,
+        minZoom: 2,
+        zoom:2,
+        zoomPosition: "bottomleft",
+        url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        marker: L.latLng(10, 0),
+        draggable: true,
+        visible: true,
+        icon: L.icon({
+           iconSize: [90, 95],
+           iconAnchor: [45, 95],
+           iconUrl: "../img/map_marker.svg",
+        })
+        }
+
+    }
+
+  }
+
+</script>
+
+<style lang="css" scoped>
+
+.full-map {
+    height: calc(100vh - 80px);
+    filter: grayscale(0.8) brightness(0.7) contrast(3) invert(0.89) hue-rotate(20deg);
+}
+</style>
