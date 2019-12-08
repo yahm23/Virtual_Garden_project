999dd5b574295a2cf0e349537c885211a99281e6
Aresky Berkane <areskyberkane@gmail.com>
2019-12-08 14:51:38 +0000

    add user seeds

--- Server/db/seeds.js
+++ Server/db/seeds.js
@@ -1,32 +1,27 @@
 use virtualGarden;
 db.dropDatabase();
-//  THIS IS JUST A SKETCH FOR THE DATABASE 
+//  THIS IS JUST A SKETCH FOR THE DATABASE
 //  I THINK THAT THE DATA FROM THE APIS SHOULD BE JUST STORED IN THE COMPONENTS
 //  IS THE DATA FROM ONE USER, ONE GARDEN AN JUST ONE PLANT
 
-// db.users.instertMany([
-//   {
-//     id: 'provided by mongodb',
-//     name:"",
-//     email:"",
-//     lastConected: "date",
-//     gardens:[
-//       {
-//         id: 'provided by mongodb',
-//         location_id:"",
-//         woeid:'number'
-//         ,
-//         plants:[
-//           {
-//             apiData: plant_id:'number',
-//             simulationData:{
-//               birthDate: "",  
-//               waterLevel:"" , 
-//             }
-//           }
-//         ]
-//       }
-//     ]
-//   }
-// ]);
 
+// INSERTING USERS
+db.users.insertMany([{
+  // AN ID FORM MONGODB
+  name: "Eduardo",
+  email: "eduardo@codeclan.co.uk",
+  lastConected: "",
+  gardens: []
+},{
+  // AN ID FORM MONGODB
+  name: "Yousef",
+  email: "yousef@codeclan.co.uk",
+  lastConected: "",
+  gardens: []
+},{
+  // AN ID FORM MONGODB
+  name: "Aresky",
+  email: "aresky@codeclan.co.uk",
+  lastConected: "",
+  gardens: []
+}]);
