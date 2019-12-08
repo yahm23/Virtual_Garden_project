a6a65fb7203b446cedab7c15f9edc8e74623c445
Aresky Berkane <areskyberkane@gmail.com>
2019-12-08 21:14:40 +0000

    add CRUD  FOR GARDEN

--- Server/helpers/create_router.js
+++ Server/helpers/create_router.js
@@ -2,80 +2,198 @@
 const express = require('express');
 const ObjectId = require('mongodb').ObjectId;
 
-const createRouter = function(collection){
+const createRouter = function(collection) {
 
   const router = express.Router();
-  //INDEX
-  router.get('/', (req,res) => {
+  //INDEX for users
+  router.get('/', (req, res) => {
     collection
-    .find()
-    .toArray()
-    .then((docs) => res.json(docs))
-    .catch((err) => {
-      console.error(err);
-      res.status(500);
-      res.json({status: 500, error: err});
-    });
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({
+          status: 500,
+          error: err
+        });
+      });
   });
 
-  //SHOW
+  //SHOW for users
   router.get('/:id', (req, res) => {
     id = req.params.id;
     collection
-    .findOne({_id:ObjectId(id) })
-    .then(doc => res.json(doc))
-    .catch( (err) => {
-      console.error(err);
-      res.status(500);
-      res.json({status: 500, error: err});
-    });
+      .findOne({
+        _id: ObjectId(id)
+      })
+      .then(doc => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({
+          status: 500,
+          error: err
+        });
+      });
   });
 
-  //POST
+  //POST for users
 
   router.post('/', (req, res) => {
-  const newData = req.body;
-  collection
-  .insertOne(newData)
-  .then((result) => {
-    res.json(result.ops[0])
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500);
-    res.json({ status: 500, error: err });
+    const newData = req.body;
+    collection
+      .insertOne(newData)
+      .then((result) => {
+        res.json(result.ops[0])
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({
+          status: 500,
+          error: err
+        });
+      });
   });
-});
 
-  //UPDATE
+  //UPDATE for users
   router.put('/:id', (req, res) => {
-  const id = req.params.id;
-  const updatedData = req.body;
-  delete updatedData._id;
-  
-  collection
-  .findOneAndUpdate({_id:ObjectId(id)}, {$set: updatedData}, {returnOriginal:false})
-  .then(result => {
-    res.json(result.value)
-  })
-  .catch((err) => {
-      res.status(500);
-      res.json({ status: 500, error: err });
-    });
-});
+    const id = req.params.id;
+    const updatedData = req.body;
+    delete updatedData._id;
 
-  //DELETE
-  router.delete('/:id', (req,res) => {
+    collection
+      .findOneAndUpdate({
+        _id: ObjectId(id)
+      }, {
+        $set: updatedData
+      }, {
+        returnOriginal: false
+      })
+      .then(result => {
+        res.json(result.value)
+      })
+      .catch((err) => {
+        res.status(500);
+        res.json({
+          status: 500,
+          error: err
+        });
+      });
+  });
+
+  //DELETE for users
+  router.delete('/:id', (req, res) => {
     const id = req.params.id;
     collection
-    .deleteOne({_id: ObjectId(id)})
-    .then(doc => res.json(doc))
-    .catch((err) => {
-      console.error(err);
-      res.status(500);
-      res.json({status: 500, error: err});
-    });
+      .deleteOne({
+        _id: ObjectId(id)
+      })
+      .then(doc => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({
+          status: 500,
+          error: err
+        });
+      });
   });
+
+  //INDEX for gardens
+  router.get('/:userId/gardens', (req, res) => {
+    id = req.params.userId;
+    collection
+      .findOne({
+        _id: ObjectId(id)
+      })
+      .then(doc => res.json(doc.gardens))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({
+          status: 500,
+          error: err
+        });
+      });
+  });
+  
+  //SHOW for gardens
+  router.get('/:userId/gardens/:location_id', (req, res) => {
+    id = req.params.userId;
+    collection
+      .findOne({
+        _id: ObjectId(id)
+      })
+      .then(doc => res.json(doc.gardens.filter(garden => {
+        return garden.location_id === req.params.location_id
+      })))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({
+          status: 500,
+          error: err
+        });
+      });
+  });
+  
+  //POST for gardens
+  router.put('/:userId/gardens', (req, res) => {
+    const id = req.params.userId;
+    const updatedData = req.body;
+    delete updatedData._id;
+
+    collection
+      .findOneAndUpdate({
+        _id: ObjectId(id)
+      }, {
+        $push: {gardens:updatedData}
+      }, {
+        returnOriginal: false
+      })
+      .then(result => {
+        res.json(result.value)
+      })
+      .catch((err) => {
+        res.status(500);
+        res.json({
+          status: 500,
+          error: err
+        });
+      });
+  });
+  
+  //DELETE for gardens
+  router.put('/:userId/gardens/:location_id/delete', (req, res) => {
+    const id = req.params.userId;
+      
+      collection
+      .findOneAndUpdate({
+        _id: ObjectId(id)
+      }, {
+        $pull: {'gardens':{location_id : req.params.location_id}}
+      }, {
+        returnOriginal: false
+      })
+      .then(result => {
+        res.json(result.value)
+      })
+      .catch((err) => {
+        res.status(500);
+        res.json({
+          status: 500,
+          error: err
+        });
+      });
+  });
+  
+    //
+
+
+
 
   return router
 };
