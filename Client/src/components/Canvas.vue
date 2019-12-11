de67da08a22806da6b62bb797515f00c5a95f841
Yousef <yahmed1996a@gmail.com>
2019-12-11 15:12:21 +0000

    P5 component fixed per plant

--- Client/src/components/Canvas.vue
+++ Client/src/components/Canvas.vue
@@ -1,9 +1,10 @@
 <template >
   <div class="">
 
-    <div v-for="item in test" >
+    <div  >
 
-      <p5></p5>
+      <p5  v-for='(plant,index) in plants' :key='index' :plant='plant'></p5>
+
     </div>
   </div>
 </template>
@@ -11,6 +12,8 @@
 <script>
 import SinglePlant from '@/components/SinglePlant.vue'
 import P5 from '@/components/P5Testing'
+import GardenServices from "../services/gardenServices"
+
 export default {
   name:"canvas-component",
   components:{
@@ -19,8 +22,13 @@ export default {
   },
     data(){
       return{
-        test:[1]
+        test:[1],
+        plants:[]
       }
+    },
+    mounted(){
+      GardenServices.getPlants()
+      .then(res => this.plants = res)
     }
 }
 </script>
