86d6f968524cd3f4152ea76ea6bf17b997f67efd
Aresky Berkane <areskyberkane@gmail.com>
2019-12-11 11:18:59 +0000

    moved mongo client

--- Client/src/services/gardenServices.js
+++ Client/src/services/gardenServices.js
@@ -1,29 +1,56 @@
 const baseUrl = "http://localhost:3000/garden"
 
-export default{
+export default {
+  //***************************
+  //PLANT SERVICES
   
-  getPlants(){
-    return fetch(baseUrl).then(res => res.json());
+  //INDEX 
+  getPlants() {
+    return fetch(baseUrl)
+      .then(res => res.json());
+  },
+  
+  //SHOW
+    showPlant(id){
+      return fetch(baseUrl + id )
+      .then(res => res.json());
     },
+  //ADD PLANT
   
-  addPlant(payload){
-    fetch(baseUrl,{
+  addPlant(payload) {
+    return fetch(baseUrl, {
       method: "POST",
       body: JSON.stringify(payload),
-      headers: { 'Content-Type': 'application/json'}
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    
+  },
+  
+  //UPDATE
+  updateP(payload) {
+    fetch(baseUrl + payload.id, {
+      method: "PUT",
+      body: JSON.stringify(payload),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }).then(res => res.json())
   },
 
-    getWeatherApi(payload){
-      return fetch(`/getweather/${payload.lat}/${payload.lng}`
-      )
-      .then(res => console.log(res));
-    },
+//*******************
 
-    getLastEnvironment(){
-      return fetch(baseUrl)
+  getWeatherApi(payload) {
+    return fetch(`/getweather/${payload.lat}/${payload.lng}`)
+      .then(res => console.log(res));
+  },
+
+  getLastEnvironment() {
+    return fetch(baseUrl)
       .then(res => res.json());
-    }
+  }
 
 
 }
--- Server/server.js
+++ Server/server.js
@@ -10,6 +10,51 @@ const fetch = require('node-fetch');
 
 app.use(parser.json());
 
+
+
+app.get('/weather/:lat/:lng', (req, res) => {
+  const lat = req.params.lat;
+  const lng = req.params.lng;
+  // console.log('latand lang',[lat,lng]);
+  const url = `https://www.metaweather.com/api/location/search/?lattlong=${lat},${lng}`
+  // console.log(url);
+  fetch(url)
+  .then(jsonData => jsonData.json())
+  .then(data => res.json(data))
+})
+app.get('/woeid/:woeid', (req, res) => {
+  const woeid = req.params.woeid
+  const url = `https://www.metaweather.com/api/location/${woeid}`
+  // console.log(url);
+  fetch(url)
+  .then(jsonData => jsonData.json())
+  .then(data => res.json(data))
+})
+
+app.get('/specificplant/:id', (req, res) => {
+  const id = req.params.id
+  const url = `https://trefle.io/api/plants/${id}/?token=Sk1pZTUyTDVMWCtRaVcyaVpBbFl1QT09`
+  // console.log(url);
+  fetch(url)
+  .then(jsonData => jsonData.json())
+  .then(data => res.json(data))
+})
+
+app.get('/plants/:temp/:humidity', (req, res) => {
+  const temp = req.params.temp
+  const humidity = req.params.humidity
+
+  const url = `https://trefle.io/api/plants/?token=Sk1pZTUyTDVMWCtRaVcyaVpBbFl1QT09&is_main_species=!null&temperature_minimum_deg_f%3E${temp}&precipitation_minimum%3E${humidity}`
+
+  // console.log(url);
+  fetch(url)
+  .then(jsonData => jsonData.json())
+  .then(data => {res.json(data)
+
+  // console.log(data)
+})
+})
+
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
 
@@ -25,45 +70,3 @@ MongoClient.connect('mongodb://localhost:27017')
 app.listen(3000, function(){
   console.log(`Listening on port ${this.address().port}`);
 });
-
-app.get('/weather/:lat/:lng', (req, res) => {
-  const lat = req.params.lat;
-  const lng = req.params.lng;
-  console.log('latand lang',[lat,lng]);
-  const url = `https://www.metaweather.com/api/location/search/?lattlong=${lat},${lng}`
-  console.log(url);
-  fetch(url)
-  .then(jsonData => jsonData.json())
-  .then(data => res.json(data))
-})
-app.get('/woeid/:woeid', (req, res) => {
-  const woeid = req.params.woeid
-  const url = `https://www.metaweather.com/api/location/${woeid}`
-  console.log(url);
-  fetch(url)
-  .then(jsonData => jsonData.json())
-  .then(data => res.json(data))
-})
-
-app.get('/specificplant/:id', (req, res) => {
-  const id = req.params.id
-  const url = `https://trefle.io/api/plants/${id}/?token=Sk1pZTUyTDVMWCtRaVcyaVpBbFl1QT09`
-  console.log(url);
-  fetch(url)
-  .then(jsonData => jsonData.json())
-  .then(data => res.json(data))
-})
-
-app.get('/plants/:temp/:humidity', (req, res) => {
-  const temp = req.params.temp
-  const humidity = req.params.humidity
-
-  const url = `https://trefle.io/api/plants/?token=Sk1pZTUyTDVMWCtRaVcyaVpBbFl1QT09&is_main_species=!null&temperature_minimum_deg_f%3E${temp}&precipitation_minimum%3E${humidity}`
-
-  console.log(url);
-  fetch(url)
-  .then(jsonData => jsonData.json())
-  .then(data => {res.json(data)
-
-  console.log(data)})
-})
